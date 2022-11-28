2eaf7481c906247ee3be8bcc37895238c1da96a8
hieupham20701 <19519511.hieu@student.iuh.edu.vn>
2022-11-28 14:17:48 +0700

    update modify user service

--- src/main/java/com/tindy/app/service/UserService.java
+++ src/main/java/com/tindy/app/service/UserService.java
@@ -4,10 +4,12 @@ import com.tindy.app.dto.request.UserRequest;
 import com.tindy.app.dto.respone.UserRespone;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+
 public interface UserService {
     UserRespone getUserInfo(Integer id);
     UserRespone uploadFile(MultipartFile multipartFile, Integer userId);
-    UserRespone updateUser(UserRequest userRequest, Integer userId);
+    UserRespone updateUser(String fullName, Integer userId, MultipartFile multipartFile) throws IOException;
     UserRespone getUserInfoByPhone(String phone);
     Boolean changeForgetPassword(String phone);
     UserRespone changePassword(String phone, String oldPassword,String newPassword);
